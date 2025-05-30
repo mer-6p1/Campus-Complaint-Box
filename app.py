83bd64d03f00d07843c5a188e44765c8cb98e55e
yohi43 <yohinamele@gmail.com>
2025-05-31 00:12:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ def save_complaint(data):
     complaints[complaint_id] = data
     with open(COMPLAINT_FILE, "w") as f:
         json.dump(complaints, f, indent=4)
+
+
+#submit complaint
+
+if menu == "Submit Complaint":
+    st.subheader("Submit a Complaint")
+    complaint = st.text_area("Enter your complaint")
+    category = st.selectbox("Select category", CATEGORIES)
+    keyword = st.text_input("Create a secret keyword (used to view your complaints later)")
